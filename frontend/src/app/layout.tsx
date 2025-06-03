d102431262b0121e9b076bbbbdc9cf996f8d3133
Arinji2 <arinjaydhar205@gmail.com>
2025-06-03 08:44:56 +0530

    Make content centered

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${ecazar.className} antialiased bg-[url('/backdrop.png')] bg-repeat w-full min-h-screen`}
+        className={`${ecazar.className} antialiased bg-[url('/backdrop.png')] flex flex-col items-center justify-center bg-repeat w-full min-h-screen`}
         style={{ backgroundSize: "auto" }}
       >
         <div className="w-full min-h-screen max-w-pageMax">{children}</div>
