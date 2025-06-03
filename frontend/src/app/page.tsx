476ba232201afd456d396aa2043a2fbf777c3ef1
Arinji2 <arinjaydhar205@gmail.com>
2025-06-03 18:30:12 +0530

    Add FeaturedRelease

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./about";
 import Hero from "./hero";
+import FeaturedRelease from "./release";
 
 export default function Home() {
   return (
     <div className=" gap-20 flex flex-col items-center justify-start py-10">
       <Hero />
       <About />
+      <FeaturedRelease />
     </div>
   );
 }
---
+++ frontend/src/app/release.tsx
@@ -0,0 +1,36 @@
+import { MusicCard } from "@/components/music/music";
+import Image from "next/image";
+
+export default function FeaturedRelease() {
+  return (
+    <div className="w-fit  gap-10 relative flex flex-col items-end px-4 justify-center">
+      <h2 className="text-4xl font-bold tracking-wider text-primary">
+        FEATURED RELEASE
+      </h2>
+      <div className="flex flex-row gap-20 justify-stretch items-center w-full md:h-[400px] mt-auto">
+        <div className="md:block aspect-video hidden w-[50%] shrink-0 overflow-hidden h-full relative ">
+          <Image
+            src="/feat-release.png"
+            fill
+            alt="Featured Release"
+            className="object-cover object-bottom"
+          />
+        </div>
+
+        <div className="w-full h-full flex flex-col items-end justify-center">
+          <MusicCard
+            item={{
+              title: "A cool song name here which is very cool",
+              id: "1234567890",
+              duration: 120,
+              originalArtists: ["Billy, Bobbie, Omkar :D"],
+              driveID: "1Pb6GA-BsOkIz39aBP4mALTFYoJRGJj4V",
+              featured: true,
+              dateAdded: new Date().toISOString(),
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
