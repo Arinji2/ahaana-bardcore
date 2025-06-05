14ec9cb000ee34e7379aeb1a13f1dd657d7c39b6
Arinji2 <arinjaydhar205@gmail.com>
2025-06-05 12:48:33 +0530

    Update Hero styles

--- frontend/src/app/hero.tsx
+++ frontend/src/app/hero.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className=" flex flex-col items-center justify-start gap-5 w-full h-[calc(100svh-280px)] md:h-[calc(100svh-40px)]">
+    <div className=" flex flex-col items-center justify-start gap-5 w-full h-[calc(100svh-40px)]">
       <h1 className="text-4xl text-center md:text-5xl lg:text-7xl font-bold text-primary">
         Cool Name Here :D
       </h1>
       <p className="text-2xl md:text-4xl text-secondary">
         A look into my music journey
       </p>
-      <Button className="tracking-normal" size="lg">
+      <Button className="text-lg" size="lg">
         View My Songs!
       </Button>
       <div className="mt-auto w-full h-full relative">
