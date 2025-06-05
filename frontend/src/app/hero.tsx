0c6e4d156930c4a6c6c2dd61ded4e736243213cb
Arinji2 <arinjaydhar205@gmail.com>
2025-06-05 09:45:44 +0530

    Update Hero Mobile Height

--- frontend/src/app/hero.tsx
+++ frontend/src/app/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className=" flex flex-col items-center justify-start gap-5 w-full h-[calc(100svh-40px)]">
+    <div className=" flex flex-col items-center justify-start gap-5 w-full h-[calc(100svh-280px)] md:h-[calc(100svh-40px)]">
       <h1 className="text-4xl text-center md:text-5xl lg:text-7xl font-bold text-primary">
         Cool Name Here :D
       </h1>
