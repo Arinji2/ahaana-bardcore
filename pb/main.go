5bfb6f5aa1871dafcae15af6e101103a2c559c5b
Arinji2 <arinjaydhar205@gmail.com>
2025-06-15 00:17:15 +0530

    Setup basic pocketbase app

---
+++ pb/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	_ "github.com/arinji2/ahaana-pb/migrations"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: isGoRun,
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
