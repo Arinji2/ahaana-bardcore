d399b9534a944421c9b95c182f4bbd089aae76d0
Arinji2 <arinjaydhar205@gmail.com>
2025-06-15 09:53:05 +0530

    Add Debug Logging

--- frontend/vercel-build-check.sh
+++ frontend/vercel-build-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo " Started at $(pwd)"
 cd ..  # Move to the actual git root
+echo "📍 Working from: $(pwd)"
 git diff HEAD^ HEAD --quiet --exit-code -- frontend/
 if [ $? -eq 0 ]; then
   echo "🛑 No changes in frontend directory, skipping build"
