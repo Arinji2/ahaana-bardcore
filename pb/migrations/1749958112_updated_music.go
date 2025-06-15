c0c17829af29a06a9e6332ebb988d025ab725bde
Arinji2 <arinjaydhar205@gmail.com>
2025-06-15 09:00:23 +0530

    Update Migrations

---
+++ pb/migrations/1749958112_updated_music.go
@@ -0,0 +1,156 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_102014113")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE INDEX ` + "`" + `idx_uzSBiGAaA9` + "`" + ` ON ` + "`" + `music` + "`" + ` (\n  ` + "`" + `title` + "`" + `,\n  ` + "`" + `pinned` + "`" + `,\n  ` + "`" + `featured` + "`" + `\n)"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text724990059",
+			"max": 0,
+			"min": 0,
+			"name": "title",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(2, []byte(`{
+			"hidden": false,
+			"id": "number2254405824",
+			"max": null,
+			"min": null,
+			"name": "duration",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text1758691358",
+			"max": 0,
+			"min": 0,
+			"name": "artists",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text1612679472",
+			"max": 0,
+			"min": 0,
+			"name": "driveID",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "bool1007901140",
+			"name": "featured",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(6, []byte(`{
+			"hidden": false,
+			"id": "bool3844597223",
+			"name": "pinned",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_102014113")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text724990059")
+
+		// remove field
+		collection.Fields.RemoveById("number2254405824")
+
+		// remove field
+		collection.Fields.RemoveById("text1758691358")
+
+		// remove field
+		collection.Fields.RemoveById("text1612679472")
+
+		// remove field
+		collection.Fields.RemoveById("bool1007901140")
+
+		// remove field
+		collection.Fields.RemoveById("bool3844597223")
+
+		return app.Save(collection)
+	})
+}
