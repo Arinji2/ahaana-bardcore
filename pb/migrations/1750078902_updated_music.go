b86df7de16e5e01f8ddb79ceff026fdfb4532f8e
Arinji2 <arinjaydhar205@gmail.com>
2025-06-16 18:49:41 +0530

    Update Migrations to match frontend format

---
+++ pb/migrations/1750078902_updated_music.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_102014113")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "date2005222912",
+			"max": "",
+			"min": "",
+			"name": "dateAdded",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "date"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_102014113")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("date2005222912")
+
+		return app.Save(collection)
+	})
+}
