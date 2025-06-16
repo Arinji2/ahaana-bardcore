6ff4ce4c860e6e5e1749706e9718396c657d941d
Arinji2 <arinjaydhar205@gmail.com>
2025-06-16 18:50:15 +0530

    Update schema to match pocketbase response

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -1,12 +1,28 @@
 import z from "zod";
 
-export const MusicSchema = z.object({
-  title: z.string(),
-  id: z.string(),
-  duration: z.number(),
-  originalArtists: z.array(z.string()),
-  driveID: z.string(),
-  featured: z.boolean(),
-  dateAdded: z.string(),
-  isOriginal: z.boolean(),
-});
+export const MusicSchema = z
+  .object({
+    id: z.string(),
+    title: z.string(),
+    duration: z.number(),
+    artists: z.string(),
+    driveID: z.string(),
+    featured: z.boolean(),
+    pinned: z.boolean(),
+    releasedOn: z.string().transform((date) => new Date(date)),
+    isOriginal: z.boolean().optional(),
+  })
+  .transform((data) => {
+    const trimmedArtists = data.artists
+      .split(",")
+      .map((a) => a.trim())
+      .filter((a) => a.length > 0);
+
+    return {
+      ...data,
+      artists: trimmedArtists,
+      isOriginal: data.isOriginal ?? trimmedArtists.length === 0,
+    };
+  });
+
+export type MusicSchemaType = z.infer<typeof MusicSchema>;
