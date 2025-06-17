da942d02bead279c17546c6348bf2f4613cc51bb
Arinji2 <arinjaydhar205@gmail.com>
2025-06-17 14:58:47 +0530

    Fix Aspect Ratio Issue

--- frontend/src/app/about.tsx
+++ frontend/src/app/about.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 export default function About() {
   return (
     <div className="w-full  gap-10 relative flex flex-col items-start px-4 justify-center">
-      <div className="w-40 md:w-60 aspect-square absolute -top-20 right-4 md:right-20">
-        <Image src="/about.png" fill alt="About" />
+      <div className="w-40 md:w-60 absolute -top-20 right-4 md:right-20">
+        <Image
+          src="/about.png"
+          alt="About"
+          width={362}
+          height={327}
+          className="w-full h-auto"
+        />
       </div>
       <h2 className="text-4xl font-bold tracking-wider text-primary">ABOUT</h2>
       <div className="flex gap-y-8 flex-col md:flex-row justify-between items-center w-full h-full mt-auto">
