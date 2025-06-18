c75ae067bdfcf00c86711550c4782c21f8ce16a7
Arinji2 <arinjaydhar205@gmail.com>
2025-06-18 23:59:26 +0530

    Add OG URL

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,10 @@ export const metadata: Metadata = {
   },
   openGraph: {
     siteName: "Ahaana Ravishankor",
+    url: new URL(
+      "/",
+      process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000",
+    ),
   },
   keywords: [
     "Ahaana",
