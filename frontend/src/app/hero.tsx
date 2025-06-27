05f660ba446516a61ca28cbf333dae4b488c7720
Arinji2 <arinjaydhar205@gmail.com>
2025-06-27 19:32:30 +0530

    Make text centre for hero

--- frontend/src/app/hero.tsx
+++ frontend/src/app/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Hero() {
   return (
-    <div className=" flex flex-col items-center justify-start gap-5 w-full h-[calc(100svh-40px)]">
+    <div className=" flex text-center flex-col items-center justify-start gap-5 w-full h-[calc(100svh-40px)]">
       <h1 className="text-4xl text-center md:text-5xl lg:text-7xl font-bold text-primary">
         Ahaana Ravishankor
       </h1>
